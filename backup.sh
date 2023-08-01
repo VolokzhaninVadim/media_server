8d5c36c848c250ea5c439f3d4b95c59e9bee3297
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-08-01 09:42:24 +0300

    change archive type

---
+++ backup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Create variables'
+FILE=$(date '+%Y-%m-%d_%H_%M_%S')
+ARCHIVE_TYPE='zst'
+GPG_TYPE='gpg'
+PROJECT='media_server'
+DIRECTORY_SOURCE='/home/volokzhanin/server/repos/media/'$PROJECT
+DIRECTORY_TARGET='/mnt/backup/backup/backup'
+DIRECTORY_S3='/mnt/s3/backup/'$PROJECT
+GPG_KEY=634064C6
+GPG_PASSPHRASE=/home/volokzhanin/.gnupg/backup_passphrase
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Create archive'
+tar --create \
+    --zstd \
+    --file=$DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE \
+    --ignore-failed-read \
+    --preserve-permissions \
+    --verbose \
+$DIRECTORY_SOURCE
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Create encrypted archive'
+which gpg
+/usr/bin/gpg -vv \
+    --recipient $GPG_KEY \
+    --symmetric \
+    --batch \
+    --passphrase-file $GPG_PASSPHRASE \
+    --no-tty \
+    --encrypt $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Remove archive'
+rm $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Move file'
+mv $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE'.'$GPG_TYPE $DIRECTORY_S3
+
+echo $(date '+%Y-%m-%d %H %M %S') 'Delete files older than n days'
+find $DIRECTORY_S3 -mtime +20 \
+    -type f \
+    -delete
